9cedaee1d0b243414098250f6401013e88c18ad3
Tuomas <tuomas.hoyla@gmail.com>
2015-12-04 00:19:19 +0200

    alusta alotettu hyva

--- webshop/shop/admin.py
+++ webshop/shop/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from models import Catalog, Basket, Product
 
 # Register your models here.
+admin.site.register(Catalog)
+admin.site.register(Basket)
+admin.site.register(Product)
