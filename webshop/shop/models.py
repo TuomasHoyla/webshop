56d7d08e30c19fe0e1c369f13aecfa1af843de12
Tuomas <tuomas.hoyla@gmail.com>
2015-12-04 00:11:35 +0200

    kuseva

--- webshop/shop/models.py
+++ webshop/shop/models.py
@@ -2,4 +2,24 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Catalog(models.Model):
+    amount = models.IntegerField(default=0)
+    product = models.ForeignKey('Product', blank=True)
+
+class Basket(models.Model):
+    amount = models.IntegerField(default=0)
+    product = models.ForeignKey('Product', blank=True)
+    
+
+class Product(models.Model):
+    
+    name = models.CharField(max_length=128, unique=True, default="product")
+    amount = models.IntegerField(default=0)
+    price = models.IntegerField(default=1)
+
+    def __unicode__(self):  #For Python 2, use __str__ on Python 3
+        return self.name
+
+
+#catalog
+#basket
--- webshop/shop/urls.py
+++ webshop/shop/urls.py
@@ -3,4 +3,7 @@ from shop import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about/', views.myView.as_view()),
+    #url(r'^products/', views.product, name='products'),
+    url(r'^testview/$', views.TestView.as_view()),
 ]
