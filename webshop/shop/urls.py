a1c3a74e99f5d0c8daa0afd8e2bb67ed998cb3cf
Tuomas <tuomas.hoyla@gmail.com>
2015-12-05 06:37:26 +0200

    restin asennus

--- webshop/shop/urls.py
+++ webshop/shop/urls.py
@@ -6,4 +6,18 @@ urlpatterns = [
     url(r'^about/', views.myView.as_view()),
     #url(r'^products/', views.product, name='products'),
     url(r'^testview/$', views.TestView.as_view()),
+    url(r'^addtobasket/', views.addToBasket, name='addtobasket'),
+    #(r'^v1/(?P<variable_a>(\d+))/(?P<variable_b>(\d+))/$', r'custom1.views.v1')
+    #url((r'^removefrombasket/(\d+)/(\d+)/$', views.removeFromBasket, name='addtobasket')),
+    url(r'^removefrombasket/(\d+)/(\d+)/$', views.removeFromBasket, name='removefrombasket'),
 ]
+
+
+#If your url is something like domain/search/?q=haha, Then you would use request.GET.get('q', '').
+#q is the parameter you want, And '' is the default value if q isn't found.
+#If you are instead just configuring your URLconf, Then your captures from the regex are passed to the function as arguments (or named arguments).
+#Such as:
+#(r'^user/(?P<username>\w{0,50})/$', views.profile_page,),
+#Then in your views.py you would have
+#def profile_page(request, username):
+    # Rest of the method
--- webshop/shop/views.py
+++ webshop/shop/views.py
@@ -2,21 +2,65 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.views.generic import View
-from shop.models import Product
+from shop.models import Product, Basket, Catalog
 from django.core import serializers
 
+
+
 def index(request):
-
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
-
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary which will be passed to the template engine.
+    product_list = Product.objects.order_by('-name')[:5]
+    context_dict = {'products': product_list}
     return render(request, 'shop/index.html', context_dict)
 
+#def addToBasket(request):
+#    print request
+#    print "hyvin tulee"
+#    return(request)
+
+#http://stackoverflow.com/questions/30295171/django-listfield-with-add-and-remove
+def addToBasket(request):
+    
+    value = request.GET.get('q','nada')
+    olio = Product.objects.get(name=value)
+    print "ajaa %d jajaj " %olio.id
+    #print value
+    basket_entry = Catalog.objects.get(product_id=olio.id)
+    print basket_entry.amount
+    basket_entry.amount = (basket_entry.amount+1)
+    basket_entry.save()
+    print basket_entry.amount
+        
+    
+    product_list = Product.objects.order_by('-name')[:5]
+    context_dict = {'products': product_list}
+    return render(request, 'shop/index.html', context_dict)
+
+#Myos taa onnistuu
+#http://127.0.0.1:8000/shop/removefrombasket/16/2/?s='erb'&e='burn'
+
+#search
+#byname
+#byamount
+#https://godjango.com/41-start-your-api-django-rest-framework-part-1/
+#http://www.django-rest-framework.org/api-guide/filtering/ for the user
+def removeFromBasket(request, a, b):
+    
+    print request
+    print a
+    print b
+    return HttpResponse(request)
+#get n kpl to basket
+    #katso onko catalogissa n kpl
+        #jos =>n
+            #lisaa n koriin
+            #poista n catalogista
+        #jos ei
+            #palauta huono homa
+        
 
 class myView(View):
     def get(self, request):
